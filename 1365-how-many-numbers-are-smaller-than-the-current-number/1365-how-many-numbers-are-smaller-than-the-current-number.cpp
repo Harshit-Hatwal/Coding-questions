8726eb1460cfe0a49bc6aeb1fc4efd2e2051ccc2
Harshit-Hatwal <64911711+Harshit-Hatwal@users.noreply.github.com>
2022-12-09 16:15:30 +0530

    Time: 12 ms (81.88%), Space: 10.5 MB (21.33%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> v;
+       unordered_map<int,int> m;
         int n=nums.size();
-        for(int i=0;i<n;i++)
-        {       int count=0;
-            for(int j=0;j<n;j++)
-            {
-                    if(j==i)
-                        continue;
-            
-                
-                if(nums[j]<nums[i])
-                        count++;
-            }
-                v.push_back(count);
-        }
+        vector<int> b=nums;
+        // done with map  traversed from  back so that for repetitive elements 
+        // the last index stored in the map corresponding to that value will be 
+        // no of elements lesser than that 
+        sort(b.begin(),b.end());
+        for(int i=n-1;i>=0;i--)
+        {
+                m[b[i]]=i;
         
-        return v;
+        }
+        //Assigning frequencies to each elements
+        for(int i= 0;i<n;i++){
+            nums[i] = m[nums[i]];
+        }
+        return nums;
         
     }
 };
