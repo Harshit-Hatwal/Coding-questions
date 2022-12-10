ef94b43f27f6e36e233873fc97910c6196e0513b
Harshit-Hatwal <64911711+Harshit-Hatwal@users.noreply.github.com>
2022-12-10 12:00:56 +0530

    Time: 481 ms (44.43%), Space: 68 MB (5.06%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        unordered_map<int,bool> present,checked;
+        for(auto a:nums)
+        {
+            present[a]=true;
+        }
+        int longestconsecutivechain=0;
+        
+        for(auto a:nums)
+        {
+            if(!checked[a]  && !present[a-1])    // here we are checking whether checked[a] is already processed or not 
+                // and  whether a-1 element is present in the hasmap  or not  if it is present  then a cant be the starting 
+                // element of the sequence . 
+            {   
+                int currentchain=0;
+                int start=a;
+                
+                while(present[start])
+                {
+                    currentchain++;
+                    checked[start]=true;
+                    start++;
+                    
+                }
+                longestconsecutivechain=max(longestconsecutivechain,currentchain);
+                
+            }
+            
+            
+        }
+        return longestconsecutivechain;
+        
+        
+    }
+};
