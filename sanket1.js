0f503e6ca451655c4bf0ea0708a9225ed24a0745
Harshit-Hatwal <harshithatwal@gmail.com>
2023-07-15 16:02:52 +0530

    sanketbackend

---
+++ sanket1.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+console.log("undefined");
+// will understand the difference bw them in coercion 
+console.log("null");
+
+console.log(0);
+console.log(-0);
+
+
+// JSX stands for javascript XML.  It allows us to write HTML in react.  It makes writing easier and adding HTMl in React . 
+
+
+//JSX allows us to write HTML elements in JavaScript and place them in the DOM without any createElement()  and/or appendChild() methods.
+
+// const name = 'Josh Perez';
+// const element = <h1>Hello, {name}</h1>;
