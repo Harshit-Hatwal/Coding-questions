ec88d7a2ebcc9f2b92e156b65b560da2cf093a7f
Harshit-Hatwal <harshithatwal@gmail.com>
2023-07-15 15:57:15 +0530

    queuedata structure

---
+++ queue.cpp
@@ -0,0 +1,26 @@
+#include<queue> 
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(3);        // 3
+    q.push(1);       //   3 1 
+    q.push(2);     //    3 1  2
+     
+    q.push(4);   //   3 1 2  4      
+
+    // queue first in first out .
+
+// if it is a stack ...
+
+
+//    LIFO  . . . . 
+// 4
+// 2
+// 1
+// 3
+
+}
